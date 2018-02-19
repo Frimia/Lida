43da60f722f3c40022ab98862fa67c284475db58
Rerumu <keys3a@gmail.com>
2018-02-19 00:14:28 -0500

    Refactored Program.cs
    Arguments and everything changed, hopefully clearer now

--- Source/Program.cs
+++ Source/Program.cs
@@ -4,7 +4,6 @@ using Relua;
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 
 /* > Rerumu
  * Developer note:
@@ -24,19 +23,23 @@ using System.Text;
 // Program.cs : Only makes usage of the Ui and compiling, Rerude itself is only dependant on the core
 namespace Rerulsd {
 	static class Program { // This is for testing stuff, just loads in some bytecode
-		enum OutputType {
-			COMPILE,
-			ASSEMBLE,
-			DISASSEMBLE
-		}
-
 		static DisReader LuReader = new DisReader();
 
+		static byte Mode = 2;
+		static bool Strip = false;
+		static bool Rebuild = false;
+		static string OutputName = null;
+
 		static byte[] Compile(string FileName) {
+			byte[] Code = File.ReadAllBytes(FileName);
+
+			if ((Code.Length == 0) || (Code[0] == 27))
+				return Code;
+
 			var Proc = new Process {
 				StartInfo = new ProcessStartInfo {
 					FileName = "luac.exe",
-					Arguments = $"-o {FileName}.out {FileName}",
+					Arguments = $"-o {FileName}.tempout {FileName}",
 					UseShellExecute = false,
 					RedirectStandardOutput = false,
 					CreateNoWindow = true
@@ -46,30 +49,10 @@ namespace Rerulsd {
 			Proc.Start();
 			Proc.WaitForExit();
 
-			return File.ReadAllBytes(FileName + ".out");
-		}
+			Code = File.ReadAllBytes(FileName + ".tempout");
+			File.Delete(FileName + ".tempout");
 
-		static byte[] AsSource(OutputType Type, byte[] Bytecode) { // This is a mess but readable ok
-			LuaProto_S Prototype = new LuaProto_S(Bytecode);
-			string Source;
-
-			switch (Type) {
-				case OutputType.COMPILE:
-					//Prototype.StripDebug(true);
-					Prototype.Analyze(true);
-
-					return new LuaProto_D(Prototype).Dump();
-				case OutputType.DISASSEMBLE:
-					LuaDisassembly Disas = new LuaDisassembly(Prototype);
-
-					Source = Disas.GetSource(0);
-
-					break;
-				default:
-					throw new NotImplementedException();
-			}
-
-			return Data.RealASCII.GetBytes(Source);
+			return Code;
 		}
 
 		static void ToFile(byte[] Content, string Path) {
@@ -85,108 +68,130 @@ namespace Rerulsd {
 			Dump.Close();
 		}
 
-		static byte[] FileBytes(string FileName) {
-			byte[] Bytecode = File.ReadAllBytes(FileName);
-
-			if (Bytecode.Length == 0)
-				throw new Exception("File was empty");
-
-			if (Bytecode[0] != 27)
-				Bytecode = Compile(FileName);
-
-			return Bytecode;
-		}
-
-		static void WriteLine(object Message, ConsoleColor Color = ConsoleColor.White, bool NewLine = true) {
+		static void WriteLine(object Message, ConsoleColor Color = ConsoleColor.Gray, bool NewLine = true) {
 			Console.ForegroundColor = Color;
 			Console.Write(Message + (NewLine ? "\n" : ""));
 			Console.ForegroundColor = ConsoleColor.Gray;
 		}
 
-		static void Commandlined(string Method, string Input, string Output) {
-			OutputType Type;
-			byte[] Files;
+		static void Assemble(string FileName) {
+			string[] Lines = File.ReadAllLines(FileName, Data.RealASCII);
+			byte[] Result = LuReader.AsArray(Lines);
 
-			switch (Method) {
-				case "cpl":
-					Type = OutputType.COMPILE;
+			ToFile(Result, OutputName ?? (FileName + ".out"));
+		}
+
+		static void Execute(string FileName) {
+			byte[] Result;
+			byte[] Bytecode = Compile(FileName);
+			LuaProto_S Proto = new LuaProto_S(Bytecode);
+
+			if (Strip)
+				Proto.StripDebug(true);
+
+			if (Rebuild)
+				Proto.Analyze(true);
+
+			WriteLine($"Size - {Bytecode.Length} bytes", ConsoleColor.Yellow);
+
+			switch (Mode) {
+				case 0:
+					Result = Bytecode;
 
 					break;
-				case "asm":
-					Type = OutputType.ASSEMBLE;
+				case 2:
+					LuaDisassembly Disas = new LuaDisassembly(Proto);
 
-					break;
-				case "dis":
-					Type = OutputType.DISASSEMBLE;
+					Result = Data.RealASCII.GetBytes(Disas.GetSource(0));
 
 					break;
 				default:
-					throw new ArgumentException("Parameter not recognized", "Method");
+					throw new ArgumentOutOfRangeException("Mode");
 			}
 
-			if (Type == OutputType.ASSEMBLE) {
-				string[] Lines = File.ReadAllLines(Input, Data.RealASCII);
+			ToFile(Result, OutputName ?? (FileName + ".out"));
+		}
 
-				Files = LuReader.AsArray(Lines);
-				WriteLine($"Bytecode size of {Files.LongLength} bytes", ConsoleColor.Yellow);
+		static void HandleError(string Input) {
+			Stopwatch Watcher = new Stopwatch();
+			bool Executed = false;
+
+#if IS_TRY_CATCH
+			try {
+#endif
+				Watcher.Start();
+
+				if (Mode != 1)
+					Execute(Input);
+				else
+					Assemble(Input);
+
+				Executed = true;
+#if IS_TRY_CATCH
 			}
-			else {
-				Files = FileBytes(Input);
-
-				WriteLine($"Bytecode size of {Files.LongLength} bytes", ConsoleColor.Yellow);
-				Files = AsSource(Type, Files);
+			catch (Exception E) {
+				WriteLine(E.Message, ConsoleColor.Red);
+				WriteLine(E.StackTrace, ConsoleColor.White);
 			}
+#endif
+			Watcher.Stop();
 
-			ToFile(Files, Output);
+			if (Executed)
+				WriteLine($"Elapsed - {Watcher.ElapsedMilliseconds}ms", ConsoleColor.DarkYellow);
+
+#if !IS_TRY_CATCH
+			Console.ReadLine();
+#endif
 		}
 
 		static void Main(string[] Args) {
 			int Numargs = Args.Length;
-			int Iterator = 0;
-			Stopwatch Watcher;
+			bool Version = false;
 
-			if (Numargs == 0) {
-				WriteLine("Rerudec created and developed by Rerumu");
-				WriteLine("Options->");
-				WriteLine("Cpl  - Compile a script", ConsoleColor.Gray);
-				WriteLine("Asm  - Assemble a disassembled file to bytecode", ConsoleColor.Gray);
-				WriteLine("Dis  - Disassemble a file to Lua assembly", ConsoleColor.Gray);
+			if (Numargs != 0) {
+				for (int Idx = 0; Idx < Numargs; Idx++) {
+					string Arg = Args[Idx];
 
-				return;
-			}
+					switch (Arg) {
+						case "-v":
+							if (!Version) {
+								WriteLine("Lida Test Build - Copyright (c) 2017 Rerumu");
 
-			while (Numargs != 0) {
-				if (Numargs == 1)
-					throw new Exception("Missing args `In` and `Out`");
-				else if (Numargs == 2)
-					throw new Exception("Missing arg `Out`");
+								Version = true;
+							}
 
-				Watcher = new Stopwatch(); // Remember to include System assembly if this errors
-				Watcher.Start();
+							break;
+						case "-o":
+							OutputName = Args[++Idx];
 
-#if IS_TRY_CATCH
-				try {
-#endif
-					Commandlined(Args[Iterator].ToLower(), Args[Iterator + 1], Args[Iterator + 2]);
+							break;
+						case "-m":
+							Mode = Byte.Parse(Args[++Idx]);
 
-					Watcher.Stop();
+							break;
+						case "-s":
+							Strip = !Strip;
 
-					WriteLine(String.Join(" ", Args[Iterator], Args[Iterator + 1], Args[Iterator + 2]), ConsoleColor.DarkCyan);
-					WriteLine($"Operation took {Watcher.ElapsedMilliseconds}ms");
-#if IS_TRY_CATCH
+							break;
+						case "-r":
+							Rebuild = !Rebuild;
+
+							break;
+						default:
+							HandleError(Arg);
+
+							break;
+					}
 				}
-				catch (Exception E) {
-					Watcher.Stop();
-					WriteLine(E.Message, ConsoleColor.Red);
-					WriteLine(E.StackTrace, ConsoleColor.White);
-				}
-#endif
-				Numargs -= 3;
-				Iterator += 3;
 			}
-#if !IS_TRY_CATCH
-			Console.ReadLine(); // Yield for testing
-#endif
+			else {
+				WriteLine("Lida options;", ConsoleColor.White);
+				WriteLine("-v          : List current version");
+				WriteLine("-o [output] : Sets the output destination");
+				WriteLine("-m [mode]   : Sets the mode, Compile = 0, Disassemble = 1, Assemble = 2");
+				WriteLine("-s          : Strips debug data from result");
+				WriteLine("-r          : Rebuilds debug data onto result");
+			}
 		}
 	}
 }
